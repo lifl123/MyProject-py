a8769f3cb6e2ec346351f5977484cf1d218d2cf6
lifl123 <13713639562@163.com>
2018-12-14 11:28:54 +0800

    commit hello.py

---
+++ hello.py
@@ -0,0 +1,21 @@
+#coding : utf-8
+# print("hello,world!")
+
+
+# #弱类型
+# #常用的简单类型
+# a = 100 #整型数字
+# b = 11.4  #float 浮点型
+# c = "TestFan"  #字符串型，python没有字符型
+# ib = 1+5j  #复数，
+# print(a)
+# print(b)
+# print(c)
+# print(ib)
+
+
+a,b,c,d = 100,101,100.999,"testfan"
+print(a)
+print(b)
+print(c)
+print(d)
